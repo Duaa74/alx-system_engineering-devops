98cee3be1d777d469215d55b43c839e14c37a6be
Duaa74 <duaasayed74@gmail.com>
2024-01-31 05:03:59 +0300

    hh

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-	puts "Usage: ruby script.rb <string>"
-	exit 1
-  end
-  
-  # Extract the argument from the command line
-  input_string = ARGV[0]
-  
-  # Define the regular expression pattern
-  pattern = /School/
-  
-  # Use the scan method to find matches in the input string
-  matches = input_string.scan(pattern)
-  
-  # Output the matches found
-  puts matches.join
+# A Ruby script that uses a regular expression that must match School
+puts ARGV[0].scan(/School/).join
